817cab5c87f82cdbdf8cf6ea1f3386dd5d613ac5
alezm <31077802+alezm00@users.noreply.github.com>
2019-05-20 17:35:39 +0200

    done

--- azm_followUser.js
+++ azm_followUser.js
@@ -11,7 +11,36 @@ registerPlugin({
         title:"Imposta il canale in cui il bot tornerà quando non trova l'tente selezionato",
         type:'string',
         indent:1
+    },{
+        name:'azm_usertofollow',
+        title:"Imposta l'utente da seguire inserisci l'uid",
+        type:'string',
+        indent:1
     }]
 }, function(_, config, meta) {
-    
+    var event = require('event');
+    var engine = require('engine');
+    var backend = require('backend');
+    event.on("load",() => {
+        let commandCreator = require("command");
+        if (!commandCreator) {
+            engine.log("Devi avere installato command.js");
+            return;
+        }
+    })
+    if(!config) {
+        config = {}
+    }
+    event.on('clientMove',ev => {
+        if (!backend.isConnected()) return;
+        if (ev.client.isSelf()) return;
+        if (typeof ev.toChannel == 'undefined') return;
+        if (ev.toChannel == 'undefined') {
+           backend.getBotClient().moveTo(backend.getChannelByID(config.azm_defaultchannel)); 
+           return;
+        }
+        if (ev.client.uid() == config.azm_usertofollow) {
+            backend.getBotClient().moveTo(ev.toChannel);
+        }
+    })
 });
